89d3b9653e77e20e2337573610aa8a5b0b6faa94
João Guilherme <joao.sczip@hotmail.com>
2024-05-14 20:30:21 -0300

    refactor: remove useless methods and add the capacity to print the metrics in a human readable way

--- internal/models/database_metrics.go
+++ internal/models/database_metrics.go
@@ -1,36 +1,26 @@
 package models
 
-import "time"
+import (
+	"fmt"
+)
 
 type DatabaseMetrics struct {
 	TotalQueries    int
 	Failures        int
-	TotalTime       time.Duration
-	MinQueryTime    time.Duration
-	MaxQueryTime    time.Duration
-	AvgQueryTime    time.Duration
-	MedianQueryTime time.Duration
+	TotalTime       float64
+	MinQueryTime    float64
+	MaxQueryTime    float64
+	AvgQueryTime    float64
+	MedianQueryTime float64
 }
 
 func NewDatabaseMetrics() *DatabaseMetrics {
 	return &DatabaseMetrics{}
 }
 
-func (m *DatabaseMetrics) SetMinQueryTime(newQueryTime time.Duration) {
-	if newQueryTime < m.MinQueryTime || m.MinQueryTime == 0 {
-		m.MinQueryTime = newQueryTime
-	}
-}
-
-func (m *DatabaseMetrics) SetMaxQueryTime(newQueryTime time.Duration) {
-	if newQueryTime > m.MaxQueryTime {
-		m.MaxQueryTime = newQueryTime
-	}
-}
-
 func (m *DatabaseMetrics) SetAverageQueryTime() {
 	if m.TotalQueries > 0 {
-		m.AvgQueryTime = m.TotalTime / time.Duration(m.TotalQueries)
+		m.AvgQueryTime = m.TotalTime / float64(m.TotalQueries)
 	}
 }
 
@@ -38,14 +28,30 @@ func (m *DatabaseMetrics) SetMedianQueryTime() {
 	m.MedianQueryTime = m.TotalTime / 2
 }
 
-func (m *DatabaseMetrics) IncrementTotalQueries() {
+func (m *DatabaseMetrics) AddQueryTime(newQueryTime float64) {
 	m.TotalQueries++
-}
 
-func (m *DatabaseMetrics) IncrementTotalTime(newQueryTime time.Duration) {
+	if newQueryTime > m.MaxQueryTime {
+		m.MaxQueryTime = newQueryTime
+	} else if newQueryTime < m.MinQueryTime || m.MinQueryTime == 0 {
+		m.MinQueryTime = newQueryTime
+	}
+
 	m.TotalTime += newQueryTime
 }
 
 func (m *DatabaseMetrics) IncrementFailures() {
 	m.Failures++
+}
+
+func (m DatabaseMetrics) ToMap() map[string]string {
+	return map[string]string{
+		"total_queries":     fmt.Sprintf("%d", m.TotalQueries),
+		"failures":          fmt.Sprintf("%d", m.Failures),
+		"min_query_time":    fmt.Sprintf("%.2fms", m.MinQueryTime),
+		"max_query_time":    fmt.Sprintf("%.2fms", m.MaxQueryTime),
+		"avg_query_time":    fmt.Sprintf("%.2fms", m.AvgQueryTime),
+		"median_query_time": fmt.Sprintf("%.2fms", m.MedianQueryTime),
+		"total_time":        fmt.Sprintf("%.2fms", m.TotalTime),
+	}
 }
--- internal/models/database_metrics_test.go
+++ internal/models/database_metrics_test.go
@@ -2,82 +2,48 @@ package models
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDatabaseMetrics(t *testing.T) {
-	t.Run("Should set the new min query time if the new value is lower than the current one", func(t *testing.T) {
-		metrics := DatabaseMetrics{MinQueryTime: 10}
-
-		metrics.SetMinQueryTime(5)
-
-		assert.Equal(t, time.Duration(5), metrics.MinQueryTime)
-	})
-
-	t.Run("Should not set the new min query time if the new value is greater than the current one", func(t *testing.T) {
-		metrics := DatabaseMetrics{MinQueryTime: 10}
-
-		metrics.SetMinQueryTime(15)
-
-		assert.Equal(t, time.Duration(10), metrics.MinQueryTime)
-	})
-
-	t.Run("Should set the new max query time if the new value is greater than the current one", func(t *testing.T) {
-		metrics := DatabaseMetrics{MaxQueryTime: 10}
-
-		metrics.SetMaxQueryTime(15)
-
-		assert.Equal(t, time.Duration(15), metrics.MaxQueryTime)
-	})
-
-	t.Run("Should not set the new max query time if the new value is lower than the current one", func(t *testing.T) {
-		metrics := DatabaseMetrics{MaxQueryTime: 10}
-
-		metrics.SetMaxQueryTime(5)
-
-		assert.Equal(t, time.Duration(10), metrics.MaxQueryTime)
-	})
-
 	t.Run("Should set the average query time", func(t *testing.T) {
 		metrics := DatabaseMetrics{TotalQueries: 10, TotalTime: 100}
 
 		metrics.SetAverageQueryTime()
 
-		assert.Equal(t, time.Duration(10), metrics.AvgQueryTime)
+		assert.Equal(t, 10.0, metrics.AvgQueryTime)
 	})
 
 	t.Run("Should set the median query time", func(t *testing.T) {
-		metrics := DatabaseMetrics{TotalTime: 100}
+		metrics := DatabaseMetrics{TotalTime: 100, TotalQueries: 2}
 
 		metrics.SetMedianQueryTime()
 
-		assert.Equal(t, time.Duration(50), metrics.MedianQueryTime)
+		assert.Equal(t, 50.0, metrics.MedianQueryTime)
 	})
 
-	t.Run("Should increment the total queries", func(t *testing.T) {
-		metrics := DatabaseMetrics{}
+	t.Run("Adding a new query time should update the metrics", func(t *testing.T) {
+		metrics := DatabaseMetrics{
+			TotalQueries: 10,
+			TotalTime:    100.0,
+			MinQueryTime: 50.0,
+			MaxQueryTime: 60.0,
+		}
 
-		metrics.IncrementTotalQueries()
+		metrics.AddQueryTime(80.0)
 
-		assert.Equal(t, 1, metrics.TotalQueries)
+		assert.Equal(t, 11, metrics.TotalQueries)
+		assert.Equal(t, 50.0, metrics.MinQueryTime)
+		assert.Equal(t, 80.0, metrics.MaxQueryTime)
+		assert.Equal(t, 180.0, metrics.TotalTime)
 
-		metrics.IncrementTotalQueries()
+		metrics.AddQueryTime(40.0)
 
-		assert.Equal(t, 2, metrics.TotalQueries)
-	})
-
-	t.Run("Should increment the total time", func(t *testing.T) {
-		metrics := DatabaseMetrics{}
-
-		metrics.IncrementTotalTime(time.Duration(10))
-
-		assert.Equal(t, time.Duration(10), metrics.TotalTime)
-
-		metrics.IncrementTotalTime(time.Duration(20))
-
-		assert.Equal(t, time.Duration(30), metrics.TotalTime)
+		assert.Equal(t, 12, metrics.TotalQueries)
+		assert.Equal(t, 40.0, metrics.MinQueryTime)
+		assert.Equal(t, 80.0, metrics.MaxQueryTime)
+		assert.Equal(t, 220.0, metrics.TotalTime)
 	})
 
 	t.Run("Should increment the failures", func(t *testing.T) {
@@ -97,6 +63,30 @@ func TestDatabaseMetrics(t *testing.T) {
 
 		metrics.SetAverageQueryTime()
 
-		assert.Equal(t, time.Duration(0), metrics.AvgQueryTime)
+		assert.Equal(t, 0.0, metrics.AvgQueryTime)
+	})
+
+	t.Run("Printing all the metrics should format it nicely", func(t *testing.T) {
+		metrics := DatabaseMetrics{
+			TotalQueries:    10,
+			Failures:        2,
+			MinQueryTime:    10.0,
+			MaxQueryTime:    20.0,
+			AvgQueryTime:    15.0,
+			MedianQueryTime: 12.0,
+			TotalTime:       150.0,
+		}
+
+		expected := map[string]string{
+			"total_queries":     "10",
+			"failures":          "2",
+			"min_query_time":    "10.00ms",
+			"max_query_time":    "20.00ms",
+			"avg_query_time":    "15.00ms",
+			"median_query_time": "12.00ms",
+			"total_time":        "150.00ms",
+		}
+
+		assert.Equal(t, expected, metrics.ToMap())
 	})
 }
