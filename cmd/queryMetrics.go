3a712bbbb02722b81c2c3db51a9945388a0bbc5a
João Guilherme <joao.sczip@hotmail.com>
2024-05-15 08:21:43 -0300

    chore: add queryMetrics command

--- cmd/queryMetrics.go
+++ cmd/queryMetrics.go
@@ -4,8 +4,15 @@ Copyright © 2024 João Guilhermer joaogbsczip@gmail.com
 package cmd
 
 import (
+	"database/sql"
 	"fmt"
 
+	"github.com/joaosczip/timescale/configs"
+	"github.com/joaosczip/timescale/internal/commands"
+	"github.com/joaosczip/timescale/internal/common"
+	"github.com/joaosczip/timescale/internal/database/repositories"
+	"github.com/joaosczip/timescale/internal/handlers"
+	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
 )
 
@@ -17,7 +24,47 @@ var queryMetricsCmd = &cobra.Command{
 The queries will be used to generate some metrics that will evaluate the performance from both the database and the application.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("queryMetrics called")
+		config, err := configs.Load(".")
+
+		if err != nil {
+			panic(err)
+		}
+
+		db, err := sql.Open(
+			"postgres",
+			fmt.Sprintf(
+				"user=%s dbname=%s password=%s host=%s port=%d sslmode=%s",
+				config.Database.User, config.Database.Name, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.SSLModel,
+			),
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer db.Close()
+
+		db.SetMaxOpenConns(config.Database.MaxOpenConns)
+
+		err = cmd.ParseFlags(args)
+
+		if err != nil {
+			panic(err)
+		}
+
+		queryParamsPath, err := cmd.Flags().GetString("query-params-path")
+
+		if err != nil {
+			panic(err)
+		}
+
+		repository := repositories.NewCpuUsageRepository(db)
+		handler := handlers.NewCollectCpuUsageMetricsHandler(repository)
+		csvReader := common.NewQueryParamsCsvReader()
+		stdoutWriter := common.NewTableWriter()
+
+		command := commands.NewDatabaseMetricsCommand(handler, csvReader, stdoutWriter)
+		command.Run(queryParamsPath)
 	},
 }
 
